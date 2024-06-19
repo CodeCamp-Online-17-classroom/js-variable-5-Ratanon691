afa434d55727ad86214c809e7a9c78e4837b854c
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-19 17:58:01 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-// write code here
+let FullName = `Rattanon`
+let LastName = `Tunwittaya`
+let age = 24
+let address = `123 ABC BKK`
+console.log(`Full name: ${FullName} ${LastName}, Age: ${age}, Address: ${address}`)
